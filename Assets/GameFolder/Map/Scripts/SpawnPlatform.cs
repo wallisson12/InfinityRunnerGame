6811f208d6a56f3c13799f28a4843fc16cc7d021
wallisson de jesus campos <wallisson_wjc@outlook.com.br>
2023-05-23 14:30:55 -0300

    Fazendo as plataformas infinitas

--- Assets/GameFolder/Map/Scripts/SpawnPlatform.cs
+++ Assets/GameFolder/Map/Scripts/SpawnPlatform.cs
@@ -6,29 +6,68 @@ public class SpawnPlatform : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> platforms = new List<GameObject>();
-    [SerializeField]
     private float offset;
+    private Transform player;
+    private List<Transform> currentPlatforms = new List<Transform>();
+
+    public Transform currentPlatformPoint;
+    public int platformIndex;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;    
+    }
 
     void Start()
     {
         for(int i=0;i<platforms.Count;i++)
         {
-            Instantiate(platforms[i], new Vector2(i * 30f, 0f), transform.rotation);
+            GameObject p = Instantiate(platforms[Random.Range(0,platforms.Count)], new Vector2(i * 30f, 0f), transform.rotation);
+
+            //Platforms in scene
+            currentPlatforms.Add(p.transform);
+
             //Free position of the next platform
             offset += 30f;
         }
+
+        //what platform I am
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
     }
 
     
     void Update()
     {
-        
+        Move();
     }
 
-    //This method to do the pooling of the platforms
+    void Move()
+    {
+        float distance = player.position.x - currentPlatformPoint.position.x;
+
+        if (distance >= 0.5f)
+        {
+            //Pooling platform
+            Pooling(currentPlatforms[platformIndex].gameObject);
+
+            //Next platform
+            platformIndex++;
+
+            if (platformIndex >= currentPlatforms.Count)
+            {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
+
+        }
+    }
+
+    //This method do the pooling of the platforms
     void Pooling(GameObject p)
     {
         p.transform.position = new Vector2(offset,0f);
         offset += 30f;
+        
     }
 }
