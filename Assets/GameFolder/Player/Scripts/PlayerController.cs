f54f74a30b29dc2497c75550baf35da651199754
wallisson de jesus campos <wallisson_wjc@outlook.com.br>
2023-05-24 14:58:36 -0300

    Cinemachine e plataformas

--- Assets/GameFolder/Player/Scripts/PlayerController.cs
+++ Assets/GameFolder/Player/Scripts/PlayerController.cs
@@ -8,11 +8,18 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField]
     private float speed,jumpForce;
+    public bool isJumping;
 
+    [SerializeField]
+    private Animator anim;
+
+    [Header("Animations Hash")]
+    public int jump;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jump = Animator.StringToHash("Jumping");
     }
 
     void FixedUpdate()
@@ -31,9 +38,20 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
+            anim.SetBool(jump, true);
+            isJumping = true;
             rb.AddForce(new Vector2(rb.velocity.x,jumpForce),ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            isJumping = false;
+            anim.SetBool(jump,false);
         }
     }
 }
