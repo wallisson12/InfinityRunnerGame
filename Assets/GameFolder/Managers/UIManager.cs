e1bb574cc803af09f2e85442c52ccfba406b1e92
wallisson de jesus campos <wallisson_wjc@outlook.com.br>
2023-07-10 23:11:08 -0300

    Modularizando o UiManager

--- Assets/GameFolder/Managers/UIManager.cs
+++ Assets/GameFolder/Managers/UIManager.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager inst;
 
     [Header("Screens")]
-    [SerializeField] private CanvasGroup gameOverScreen,startGameScreen;
+    [SerializeField] private CanvasGroup gameOverScreen,startGameScreen,pauseScreen;
 
     [Header("Hud")]
     public TextMeshProUGUI distanceTxt,distanceHud;
     public Image shooterBar;
+
+    //--Buttons--
+    private Button playBtn, retryBtn, pauseBtn,backPauseBtn,rankBtn, menuBtn;
     
 
     void Awake()
@@ -22,7 +26,58 @@ public class UIManager : MonoBehaviour
         {
             inst = this;
         }
+
+        Load();
     }
+
+    /// <summary>
+    /// Take all buttons and add methods
+    /// </summary>
+    void Load()
+    {
+
+        //Screens
+
+        startGameScreen = GameObject.FindGameObjectWithTag("StartGameScreen").GetComponent<CanvasGroup>();
+        gameOverScreen = GameObject.FindGameObjectWithTag("GameOverScreen").GetComponent<CanvasGroup>();
+        pauseScreen = GameObject.FindGameObjectWithTag("PauseScreen").GetComponent<CanvasGroup>();
+
+
+
+        //Buttons
+
+
+        //Pause
+        pauseBtn = GameObject.FindGameObjectWithTag("PauseBtn").GetComponent<Button>();
+        backPauseBtn = GameObject.FindGameObjectWithTag("ReturnPause").GetComponent<Button>();
+
+
+        //GameOver
+        retryBtn = GameObject.FindGameObjectWithTag("RestartBtn").GetComponent<Button>();
+
+
+        //Events
+        pauseBtn.onClick.AddListener(PauseGame);
+        backPauseBtn.onClick.AddListener(BackPause);
+        retryBtn.onClick.AddListener(Retry);
+
+    }
+
+
+
+    //---Buttons Methods---
+    void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+
+
+
+
+
+    //---Screens methods---
 
     /// <summary>
     /// Start the game
@@ -43,6 +98,22 @@ public class UIManager : MonoBehaviour
         gameOverScreen.alpha = 1f;
         gameOverScreen.interactable = true;
         gameOverScreen.blocksRaycasts = true;
+    }
+
+    public void PauseGame()
+    {
+        pauseScreen.alpha = 1f;
+        pauseScreen.interactable = true;
+        pauseScreen.blocksRaycasts = true;
+        Time.timeScale = 0f;
+    }
+
+    public void BackPause()
+    {
+        pauseScreen.alpha = 0f;
+        pauseScreen.interactable = false;
+        pauseScreen.blocksRaycasts = false;
+        Time.timeScale = 1f;
     }
 
     /// <summary>
